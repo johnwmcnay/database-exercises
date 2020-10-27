ab7f1baf015155f23499fdda7f3515ba474c60f0
John McNay <john.w.mcnay@gmail.com>
2020-10-27 10:59:35 -0500

    finished first three select statements in exercise

---
+++ select_exercises.sql
@@ -0,0 +1,19 @@
+USE codeup_test_db;
+
+# The name of all albums by Pink Floyd.
+# The year Sgt. Pepper's Lonely Hearts Club Band was released
+# The genre for Nevermind
+# Which albums were released in the 1990s
+# Which albums had less than 20 million certified sales
+# All the albums with a genre of "Rock".
+# Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
+
+SELECT 'All albums by Pink Floyd' AS 'Info';
+SELECT name FROM albums WHERE artist='Pink Floyd';
+
+SELECT 'Sgt. Pepper''s release year' AS 'Info';
+SELECT release_date FROM albums WHERE name='Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT 'Genre of Nevermind' AS 'Info';
+SELECT genre FROM albums WHERE name='Nevermind';
+
